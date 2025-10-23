9899d8ae38377bb3bbdf939968fde10de6d20960
Phoebe <Phoebegittis@gmail.com>
2025-10-23 16:27:28 -0400

    10/23/25

---
+++ js-introductory-exercises/01-greeting/greeting.js
@@ -0,0 +1,12 @@
+// Learn how to make an alert: https://www.w3schools.com/jsref/met_win_alert.asp
+
+function sayHello() {
+  let messageEL = document.querySelector("#message");
+  messageEL.innerHTML = "Hello"
+}
+
+function sayGoodbye() {
+  // your code here...
+  let messageEL = document.querySelector("#message");
+  messageEL.innerHTML = "Goodbye"
+}
